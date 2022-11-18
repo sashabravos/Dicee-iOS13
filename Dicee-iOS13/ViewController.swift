3e4bc76558b482ee9b4d39bdabbbc84cbae333fd
Alexandra Kostrova <sashabravos@gmail.com>
2022-11-18 19:18:23 +0300

    final version

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,54 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //MARK: - Function
+    let allCubesImageNames = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
     
-    @objc func buttonAction(sender: UIButton!) {
-
-    }
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    //MARK: - Objects
-        
+    private lazy var backgroundImage: UIImageView = {
         let backgroundImage = UIImageView.init(image: UIImage(named: "GreenBackground")!)
         backgroundImage.contentMode = .scaleToFill
-        
+        return backgroundImage
+    }()
+    
+    private lazy var logo: UIImageView = {
         let logo = UIImageView.init(image: UIImage(named: "DiceeLogo")!)
         logo.contentMode = .scaleAspectFit
-        
-        let diceOne = UIImageView.init(image: UIImage(named: "DiceOne")!)
-        diceOne.frame = CGRect(x: Int(backgroundImage.frame.midX / 3),
-                               y: Int(backgroundImage.frame.size.height),
-                               width: 120, height: 120)
-        
-        let diceTwo = UIImageView.init(image: UIImage(named: "DiceTwo")!)
-        diceTwo.frame = CGRect(x: Int(backgroundImage.frame.midX) ,
-                               y: Int(backgroundImage.frame.size.height),
-                               width: 120, height: 120)
-        
+        return logo
+    }()
+    
+    private lazy var cubeOne: UIImageView = {
+        let cubeOne = UIImageView.init(image: getRandomImage())
+        return cubeOne
+    }()
+    
+    private lazy var cubeTwo: UIImageView = {
+        let cubeTwo = UIImageView.init(image: getRandomImage())
+        return cubeTwo
+    }()
+    
+    private lazy var rollButton: UIButton = {
         let button = UIButton()
         button.setTitle("Roll", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = UIColor(named: "buttonColor")
-        button.center = view.center
         button.layer.cornerRadius = 15
         button.layer.masksToBounds = true
         button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
         button.addTarget(self, action: #selector(self.buttonAction(sender:)), for: .touchUpInside)
-
-    // MARK: - Lifecycle
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.layoutSubviews()
+    }
+    
+    private func layoutSubviews() {
+        view.addSubview(self.backgroundImage)
+        view.addSubview(self.logo)
+        view.addSubview(self.cubeOne)
+        view.addSubview(self.cubeTwo)
+        view.addSubview(self.rollButton)
         
-        view.addSubview(backgroundImage)
-        view.addSubview(logo)
-        view.addSubview(diceOne)
-        view.addSubview(diceTwo)
-        view.addSubview(button)
-
-    //MARK: - Constraints
+        //MARK: - Constraints
         
         backgroundImage.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -73,50 +74,40 @@ class ViewController: UIViewController {
             logo.topAnchor.constraint(equalTo: backgroundImage.topAnchor, constant: 100)
         ])
         
-        button.translatesAutoresizingMaskIntoConstraints = false
+        cubeOne.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            button.leadingAnchor.constraint(equalTo: backgroundImage.leadingAnchor, constant: 120),
-            button.centerXAnchor.constraint(equalTo: backgroundImage.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: backgroundImage.centerYAnchor, constant: 250),
-            button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -170)
+            cubeOne.leadingAnchor.constraint(equalTo: backgroundImage.leadingAnchor, constant: backgroundImage.frame.midX / 3),
+            cubeOne.topAnchor.constraint(equalTo: backgroundImage.topAnchor, constant: backgroundImage.frame.size.height),
+            cubeOne.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -363),
+            cubeOne.rightAnchor.constraint(equalTo: backgroundImage.rightAnchor, constant: -223),
         ])
-
         
-
-
-        
-        
+        cubeTwo.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        cubeTwo.leadingAnchor.constraint(equalTo: backgroundImage.leadingAnchor, constant: backgroundImage.frame.midX),
+        cubeTwo.topAnchor.constraint(equalTo: cubeOne.topAnchor),
+        cubeTwo.bottomAnchor.constraint(equalTo: cubeOne.bottomAnchor),
+        cubeTwo.heightAnchor.constraint(equalTo: cubeOne.heightAnchor),
+        cubeTwo.widthAnchor.constraint(equalTo: cubeOne.widthAnchor)
+    ])
         
     
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        rollButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            rollButton.leadingAnchor.constraint(equalTo: backgroundImage.leadingAnchor, constant: 120),
+            rollButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -170),
+            rollButton.centerXAnchor.constraint(equalTo: backgroundImage.centerXAnchor),
+            rollButton.centerYAnchor.constraint(equalTo: backgroundImage.centerYAnchor, constant: 250)
+        ])
+    }
+
+        //MARK: - Function
+    @objc func buttonAction(sender: UIButton!) {
+        self.cubeOne.image = getRandomImage()
+        self.cubeTwo.image = getRandomImage()
+    }
+    
+    private func getRandomImage() -> UIImage? {
+        UIImage(named: self.allCubesImageNames.randomElement() ?? "")
     }
 }
-
-
-                
-        
-
-
-
-
-
